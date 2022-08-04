b0abdffa0e586efda42dd345f37f2275b4afa42a
Devben <benjaminalcis.xbox@gmail.com>
2022-08-03 22:21:31 -0500

    finish class02

---
+++ fundamentosDeProgramacion/class02/base.js
@@ -0,0 +1,17 @@
+let saludo = "hola Mundo, Hola Koders"
+console.log(saludo)
+
+let edad = 15
+console.log (edad == 5)
+
+let edad2 = 20
+console.log(edad < edad2)
+
+console.log(typeof saludo)
+
+console.log((((12+6)/3)+2)>6)
+console.log((((23+47)/2)+1)%2===2)
+console.log(((33/3)%2)>1||(12/3)>=1)
+console.log(3>5&&2>=3&&2>1||2>=2)
+console.log(11>2&&1<1&&-4>5||!(8>=7))
+console.log(!(11>2&&1<1&&-4>-5||!(8>=7)))
