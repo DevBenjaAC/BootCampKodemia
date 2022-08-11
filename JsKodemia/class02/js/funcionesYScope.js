0cddd31e2e4370e367b6ba05b7b5348b3a61750b
Devben <benjaminalcis.xbox@gmail.com>
2022-08-10 22:02:49 -0500

    finish class

---
+++ JsKodemia/class02/js/funcionesYScope.js
@@ -0,0 +1,26 @@
+/**
+ * FUNCIONES Y SCOPE
+ * Conjunto de expresiones y sentencias agrupadas para reutilizar codigo.
+ * 
+ * validad, construir, devolver cosas
+ * 
+ * Forma declarativa:   //Palabra Reservada
+ * function miFuncion (){
+ * return3;
+ * }
+ * 
+ * Forma de expresion:  //Funcion asiganada a variable
+ * let miFuncion = function(a, b){
+ * return a + b;}
+ */
+
+//declarativa
+function miFuncion(texto1, texto2){
+  
+  console.log(texto1 + texto2)
+}
+
+//Expresion
+let miVariableEsUnaFuntion = function(){
+
+}
