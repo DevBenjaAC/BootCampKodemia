197d3a17fa401339561873698c09deb08d02e33a
Devben <benjaminalcis.xbox@gmail.com>
2022-08-12 16:35:07 -0500

    new exersices

--- JsKodemia/class03/js/exercises.js
+++ JsKodemia/class03/js/exercises.js
@@ -12,7 +12,7 @@ let resultadoSuma = sumar(1, 4);
 console.log(resultadoSuma);
 
 /**
- * Crear una funcion que recibe  un nombre 
+ * Crear una funcion que recibe  un nombre
  * y devuelve un saludo  por nombre  y si  no se provee  el nombre saludo simplemente  por "amigo".
  */
 
@@ -25,3 +25,30 @@ console.log(saludo("cintia"));
 console.log(saludo("benja"));
 console.log(saludo(""));
 console.log(saludo());
+
+//homework
+/**
+ * 1.-Crear una funcion que altere el valor de una variable  global cada que se ejecuta (no debe retornar nada)
+ */
+
+let variableGlobal = 284;
+function alteracion() {
+  console.log(++variableGlobal);
+}
+alteracion() //Aqui variableGlobal vale 285
+alteracion() //Aqui variableGlobal vale 286
+//etc...
+
+/*
+ * 2.-Crear una funcion que reciba un parametro  de tipo numero y regrese true si el paramentro es un numero par  o false si
+ * es un numero inpar.
+ */
+//En cualquier lenguaje de programación .
+//Si el módulo a dos del número es igual a cero el número es par, en caso contrario impar.
+function esPar(number){
+  console.log((number % 2) == 0);
+}
+esPar(260) // Es par
+esPar(514) // Es par
+esPar(1083) //Es Impar
+esPar(157.9) //Es impar
