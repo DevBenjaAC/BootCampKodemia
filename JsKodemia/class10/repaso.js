8c3107f43debdf8d21d84802342d327b58062526
Devben <benjaminalcis.xbox@gmail.com>
2022-08-25 19:59:55 -0500

    33 conceptos de Javascript AHORA SI EN ESPAÑOL

---
+++ JsKodemia/class10/repaso.js
@@ -0,0 +1,9 @@
+/**
+ * Repasar filter
+ * Repasar find
+ * Investigar linters y formaters
+ * Repasar prototipe array
+ * Repasar map
+ * Repasar sort
+ */
+
---
+++ JsKodemia/practices/33ConceptosJavscript.js
@@ -0,0 +1,145 @@
+/**
+ * ## 33 conceptos de Javascript AHORA SI EN ESPAÑOL
+
+El repositorio de GitHub me parece perfecto, pero hay un problema, muchos programadores aún no perfeccionan su nivel de inglés y eso dificulta mucho el proceso de aprendizaje. Aquí me estoy basando en los mismos conceptos pero con artículos totalmente en ESPAÑOL. 
+
+Esto tiene como objetivo solo RECOPILAR la información. Ninguno de estos posts son de mi autoría. 
+
+Por el momento solo hay artículos, posteriormente añadiré videos que vaya encontrando. 
+
+1. **[Pila de llamadas](https://github.com/adonismendozaperez/33-js-conceptos#1-pila-de-llamadas)**
+    1. [https://www.itdo.com/blog/javascript-conceptos-fundamentales-la-pila-de-llamadas/](https://www.itdo.com/blog/javascript-conceptos-fundamentales-la-pila-de-llamadas/)
+    2. [https://programmerclick.com/article/4798449974/](https://programmerclick.com/article/4798449974/)
+    3. [https://es.quish.tv/understanding-javascript-call-stack](https://es.quish.tv/understanding-javascript-call-stack)
+    
+2. **[Tipos primitivos](https://github.com/adonismendozaperez/33-js-conceptos#2-tipos-primitivos)**
+    1. [https://www.escuelafrontend.com/articulos/los-tipos-primitivos-en-javascript](https://www.escuelafrontend.com/articulos/los-tipos-primitivos-en-javascript)
+    2. [https://www.netinetidesign.com/post/tipos-de-datos-en-javascript/](https://www.netinetidesign.com/post/tipos-de-datos-en-javascript/)
+    3. [https://developer.mozilla.org/es/docs/Web/JavaScript/Data_structures](https://developer.mozilla.org/es/docs/Web/JavaScript/Data_structures)
+    
+3. **[Tipos de valor y tipos de referencia](https://github.com/adonismendozaperez/33-js-conceptos#3-tipos-de-valores-y-tipos-de-referencias)**
+    1. [https://es.acervolima.com/valor-primitivo-y-de-referencia-en-javascript/](https://es.acervolima.com/valor-primitivo-y-de-referencia-en-javascript/)
+    2. [https://medium.com/laboratoria-developers/por-valor-vs-por-referencia-en-javascript-de3daf53a8b9](https://medium.com/laboratoria-developers/por-valor-vs-por-referencia-en-javascript-de3daf53a8b9)
+    
+4. **[Implícito, Explícito, Nominal, Estructuración y Escritura de pato](https://github.com/adonismendozaperez/33-js-conceptos#4-impl%C3%ADcito-expl%C3%ADcito-nominal-estructuraci%C3%B3n-y-escritura-de-pato)**
+    1. [http://www.etnassoft.com/2011/04/06/coercion-de-datos-en-javascript/](http://www.etnassoft.com/2011/04/06/coercion-de-datos-en-javascript/)
+    2. [https://dev.to/manucastrillonm/entendiendo-la-coercion-en-javascript-22l8](https://dev.to/manucastrillonm/entendiendo-la-coercion-en-javascript-22l8)
+    
+5. **[== vs === vs typeof](https://github.com/adonismendozaperez/33-js-conceptos#5--vs--vs-typeof)**
+    1. [https://runebook.dev/es/docs/javascript/operators/typeof](https://runebook.dev/es/docs/javascript/operators/typeof)
+    2. [https://www.freecodecamp.org/espanol/news/triple-igual-vs-doble-igual-en-javascript-comparison-operators-explained-with-examples/](https://www.freecodecamp.org/espanol/news/triple-igual-vs-doble-igual-en-javascript-comparison-operators-explained-with-examples/)
+    
+6. **[Alcance de la función, Ámbito de bloque y alcance léxico](https://github.com/adonismendozaperez/33-js-conceptos#6-alcance-de-la-funci%C3%B3n-%C3%A1mbito-de-bloque-y-alcance-l%C3%A9xico)**
+    1. [https://dev.to/isram/como-funciona-el-scope-de-javascript-con-las-variables-var-let-y-const-en-10-minutos-4009](https://dev.to/isram/como-funciona-el-scope-de-javascript-con-las-variables-var-let-y-const-en-10-minutos-4009)
+    2. [https://medium.com/@sergiodxa/definiendo-conceptos-closure-y-scope-en-javascript-9081f1e113e6](https://medium.com/@sergiodxa/definiendo-conceptos-closure-y-scope-en-javascript-9081f1e113e6)
+    
+7. **[Expression vs Statement](https://github.com/adonismendozaperez/33-js-conceptos#7-expression-vs-statement)**
+    1. [https://tech-wiki.online/es/javascript-statements.html](https://tech-wiki.online/es/javascript-statements.html)
+    2. [https://notasjs.blogspot.com/2013/08/javascript-diferencia-entre-expresion-y.html](https://notasjs.blogspot.com/2013/08/javascript-diferencia-entre-expresion-y.html)
+    3. [https://es.javascript.info/function-expressions](https://es.javascript.info/function-expressions)
+    
+8. **[IIFE, módulos y espacios de nombres](https://github.com/adonismendozaperez/33-js-conceptos#8-iife-m%C3%B3dulos-y-espacios-de-nombres)**
+    1. [https://developer.mozilla.org/es/docs/Glossary/IIFE](https://developer.mozilla.org/es/docs/Glossary/IIFE)
+    2. [https://somospnt.com/blog/274-tipos-de-funciones-en-javascript](https://somospnt.com/blog/274-tipos-de-funciones-en-javascript)
+    3. [https://programacionymas.com/blog/funciones-javascript-invocadas-inmediatamente-IIFE](https://programacionymas.com/blog/funciones-javascript-invocadas-inmediatamente-IIFE)
+    
+9. **[Message Queue y Event Loop](https://github.com/adonismendozaperez/33-js-conceptos#9-message-queue-y-event-loop)**
+    1. [https://davidjsmoreno.medium.com/entendiendo-el-event-loop-o-bucle-de-eventos-en-javascript-b294d882fd14](https://davidjsmoreno.medium.com/entendiendo-el-event-loop-o-bucle-de-eventos-en-javascript-b294d882fd14)
+    2. [https://developer.mozilla.org/es/docs/Web/JavaScript/EventLoop](https://developer.mozilla.org/es/docs/Web/JavaScript/EventLoop)
+    
+10. **[setTimeout, setInterval y requestAnimationFrame](https://github.com/adonismendozaperez/33-js-conceptos#10-settimeout-setinterval-y-requestanimationframe)**
+    1. [https://es.javascript.info/settimeout-setinterval](https://es.javascript.info/settimeout-setinterval)
+    2. [https://developer.mozilla.org/es/docs/Web/API/Window/requestAnimationFrame](https://developer.mozilla.org/es/docs/Web/API/Window/requestAnimationFrame)
+    
+11. **[Motores de JavaScript](https://github.com/adonismendozaperez/33-js-conceptos#11-motores-de-JavaScript)**
+    1. [https://mauriciogc.medium.com/javascript-historia-estándar-motores-aeb453d9d9e](https://mauriciogc.medium.com/javascript-historia-est%C3%A1ndar-motores-aeb453d9d9e)
+    
+12. **[Bitwise Operators, Type Arrays y Array Buffers](https://github.com/adonismendozaperez/33-js-conceptos#12-bitwise-operators-type-arrays-y-array-buffers)**
+    1. [https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Expressions_and_Operators#bit_a_bit](https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Expressions_and_Operators#bit_a_bit)
+    2. [https://programmerclick.com/article/58591737792/](https://programmerclick.com/article/58591737792/)
+    3. [https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array)
+    4. [https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer)
+    5. [https://es.javascript.info/arraybuffer-binary-arrays](https://es.javascript.info/arraybuffer-binary-arrays)
+    
+13. **[DOM y Layout Trees](https://github.com/adonismendozaperez/33-js-conceptos#13-dom-y-layout-trees)**
+    1. [https://www.freecodecamp.org/espanol/news/que-es-el-dom-el-significado-del-modelo-de-objeto-de-documento-en-javascript/](https://www.freecodecamp.org/espanol/news/que-es-el-dom-el-significado-del-modelo-de-objeto-de-documento-en-javascript/)
+    2. [https://developer.mozilla.org/es/docs/Web/API/Document_Object_Model/Introduction](https://developer.mozilla.org/es/docs/Web/API/Document_Object_Model/Introduction)
+    3. [https://codigofacilito.com/articulos/javascript-dom-desarrollo-web](https://codigofacilito.com/articulos/javascript-dom-desarrollo-web)
+    
+14. **[Factories y Classes](https://github.com/adonismendozaperez/33-js-conceptos#14-factories-y-classes)**
+    1. [https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Classes](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Classes)
+    2. [https://es.javascript.info/classes](https://es.javascript.info/classes) Aqui hay bastante info de clases igual.
+    
+15. **[this, call, apply y bind](https://github.com/adonismendozaperez/33-js-conceptos#15-this-call-apply-y-bind)**
+    1. [https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/this](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/this)
+    2. [https://yeisondaza.com/entendiendo-this-javascript](https://yeisondaza.com/entendiendo-this-javascript)
+    3. [https://www.freecodecamp.org/espanol/news/la-guia-completa-sobre-this-en-javascript/](https://www.freecodecamp.org/espanol/news/la-guia-completa-sobre-this-en-javascript/)
+    
+16. **[new, Constructor, instanceof y Instances](https://github.com/adonismendozaperez/33-js-conceptos#16-new-constructor-instanceof-y-instances)**
+    1. [https://geeks.ms/etomas/2015/10/15/qu-hace-new-en-javascript/](https://geeks.ms/etomas/2015/10/15/qu-hace-new-en-javascript/)
+    2. [https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Classes/constructor](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Classes/constructor)
+    
+17. **[Prototype Inheritance y Prototype Chain](https://github.com/adonismendozaperez/33-js-conceptos#17-prototype-inheritance-y-prototype-chain)**
+    1. [https://developer.mozilla.org/es/docs/Web/JavaScript/Inheritance_and_the_prototype_chain](https://developer.mozilla.org/es/docs/Web/JavaScript/Inheritance_and_the_prototype_chain)
+    2. [https://www.freecodecamp.org/espanol/news/prototipo-javascript-expliacado-con-ejemplos/](https://www.freecodecamp.org/espanol/news/prototipo-javascript-expliacado-con-ejemplos/)
+    3. [https://ricardogeek.com/un-vistazo-a-los-prototipos-de-javascript/](https://ricardogeek.com/un-vistazo-a-los-prototipos-de-javascript/)
+    
+18. **[Object.create y Object.assign](https://github.com/adonismendozaperez/33-js-conceptos#18-objectcreate-y-objectassign)**
+    1. [https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object/create](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object/create)
+    2. [https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object/assign](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object/assign)
+    
+19. **[map, reduce, filter](https://github.com/adonismendozaperez/33-js-conceptos#19-map-reduce-filter)**
+    1. [https://code.tutsplus.com/es/tutorials/how-to-use-map-filter-reduce-in-javascript--cms-26209](https://code.tutsplus.com/es/tutorials/how-to-use-map-filter-reduce-in-javascript--cms-26209)
+    2. [https://platzi.com/blog/uso-de-filter-map-y-reduce/](https://platzi.com/blog/uso-de-filter-map-y-reduce/)
+    3. [https://hatthieves.es/2019/08/29/map-filter-y-reduce-javascript/](https://hatthieves.es/2019/08/29/map-filter-y-reduce-javascript/)
+    
+20. **[Funciones puras, efectos secundarios y mutación de estado](https://github.com/adonismendozaperez/33-js-conceptos#20-Funciones-puras-efectos-secundarios-y-mutaci%C3%B3n-de-estado)**
+    1. [https://medium.com/laboratoria-developers/introducción-a-la-programación-funcional-en-javascript-parte-2-funciones-puras-b99e08c2895d](https://medium.com/laboratoria-developers/introducci%C3%B3n-a-la-programaci%C3%B3n-funcional-en-javascript-parte-2-funciones-puras-b99e08c2895d)
+    
+21. **[Closures](https://github.com/adonismendozaperez/33-js-conceptos#21-closures)**
+    1. [https://www.freecodecamp.org/espanol/news/que-es-un-closure-en-javascript/](https://www.freecodecamp.org/espanol/news/que-es-un-closure-en-javascript/)
+    
+22. **[High Order Functions](https://github.com/adonismendozaperez/33-js-conceptos#22-high-order-functions)**
+    1. [https://medium.com/entendiendo-javascript/funciones-de-alto-orden-en-javascript-42d04769d9b5](https://medium.com/entendiendo-javascript/funciones-de-alto-orden-en-javascript-42d04769d9b5)
+    
+23. **[Recursion](https://github.com/adonismendozaperez/33-js-conceptos#23-recursion)**
+    1. [https://www.freecodecamp.org/espanol/news/como-entender-recursividad-en-javascript/](https://www.freecodecamp.org/espanol/news/como-entender-recursividad-en-javascript/)
+    
+24. **[Collections y Generators](https://github.com/adonismendozaperez/33-js-conceptos#24-collections-y-generators)**
+    1. [https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Iterators_and_Generators](https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Iterators_and_Generators)
+    
+25. **[Promesas](https://github.com/adonismendozaperez/33-js-conceptos#25-promesas)**
+    1. [https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Using_promises](https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Using_promises)
+    2. [https://www.freecodecamp.org/espanol/news/que-es-una-promesa-promesas-de-javascript-para-principiantes/](https://www.freecodecamp.org/espanol/news/que-es-una-promesa-promesas-de-javascript-para-principiantes/)
+    
+26. **[async/await](https://github.com/adonismendozaperez/33-js-conceptos#26-asyncawait)**
+    1. [https://lenguajejs.com/javascript/asincronia/async-await/](https://lenguajejs.com/javascript/asincronia/async-await/)
+    
+27. **[Estructuras de datos](https://github.com/adonismendozaperez/33-js-conceptos#27-estructuras-de-datos)**
+    1. [https://medium.com/noders/estructuras-de-datos-con-javascript-parte-1-pilas-stacks-5a2092cff16a](https://medium.com/noders/estructuras-de-datos-con-javascript-parte-1-pilas-stacks-5a2092cff16a)
+    2. [https://alexmarket.medium.com/estructuras-de-datos-en-javascript-5426123bff50](https://alexmarket.medium.com/estructuras-de-datos-en-javascript-5426123bff50)
+    
+28. **[Expensive Operation y Big O Notation](https://github.com/adonismendozaperez/33-js-conceptos#28-expensive-operation-y-big-o-notation)**
+    1. [https://medium.com/@charlie_fuentes/notacion-big-0-para-principiantes-f9cbb4b6bec8](https://medium.com/@charlie_fuentes/notacion-big-0-para-principiantes-f9cbb4b6bec8)
+    
+29. **[Algoritmos](https://github.com/adonismendozaperez/33-js-conceptos#29-algoritmos)**
+    1. [https://medium.com/@juanelojga/algoritmos-en-javascript-parte-1-47a0b13db35a](https://medium.com/@juanelojga/algoritmos-en-javascript-parte-1-47a0b13db35a)
+    2. [https://github.com/trekhleb/javascript-algorithms/blob/master/README.es-ES.md](https://github.com/trekhleb/javascript-algorithms/blob/master/README.es-ES.md)
+    
+30. **[Herencia, polimorfismo y reutilización de código](https://github.com/adonismendozaperez/33-js-conceptos#30-herencia-polimorfismo-y-reutilizaci%C3%B3n-de-c%C3%B3digo)**
+    1. [https://desarrolloweb.com/articulos/polimorfismo-programacion-orientada-objetos-concepto.html](https://desarrolloweb.com/articulos/polimorfismo-programacion-orientada-objetos-concepto.html)
+    2. [https://code.tutsplus.com/es/tutorials/inheritance-and-extending-objects-with-javascript--cms-29836](https://code.tutsplus.com/es/tutorials/inheritance-and-extending-objects-with-javascript--cms-29836)
+    
+31. **[Patrones de diseño](https://github.com/adonismendozaperez/33-js-conceptos#31-patrones-de-dise%C3%B1o)**
+    1. [https://medium.com/@jmz12/patrones-de-diseño-en-js-43beab8f5756](https://medium.com/@jmz12/patrones-de-dise%C3%B1o-en-js-43beab8f5756)
+    2. [https://github.com/Fictizia/Curso-JS-Avanzado-para-desarrolladores-Front-end_ed3/blob/master/teoria/clase17.md](https://github.com/Fictizia/Curso-JS-Avanzado-para-desarrolladores-Front-end_ed3/blob/master/teoria/clase17.md)
+    
+32. **[Partial Applications, Currying, Compose y Pipe](https://github.com/adonismendozaperez/33-js-conceptos#32-partial-applications-currying-compose-y-pipe)**
+    1. [https://dev.to/vonheikemen/un-poco-del-paradigma-funcional-en-tu-javascript-aplicacion-parcial-41kl](https://dev.to/vonheikemen/un-poco-del-paradigma-funcional-en-tu-javascript-aplicacion-parcial-41kl)
+    2. [https://www.nicolas-schurmann.com/aplicacion-parcial-y-currying/](https://www.nicolas-schurmann.com/aplicacion-parcial-y-currying/)
+    
+33. **[Código limpio](https://github.com/adonismendozaperez/33-js-conceptos#33-c%C3%B3digo-limpio)**
+    1. [https://www.syntonize.com/10-claves-javascript-codigo-limpio/](https://www.syntonize.com/10-claves-javascript-codigo-limpio/)
+    2. [https://todoxampp.com/codigo-limpio-en-javascript-y-buenas-practicas/](https://todoxampp.com/codigo-limpio-en-javascript-y-buenas-practicas/)
+--Autor:Rodrigo Montoya
+    */
