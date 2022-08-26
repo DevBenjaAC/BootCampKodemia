ed6c291073610e5d6bb9fa9f6ff7387ef236b6da
Devben <benjaminalcis.xbox@gmail.com>
2022-08-25 22:06:42 -0500

    new class

--- JsKodemia/class10/repaso.js
+++ JsKodemia/class10/repaso.js
@@ -5,5 +5,9 @@
  * Repasar prototipe array
  * Repasar map
  * Repasar sort
- */
+ * objeto Literal
+ * Expresion en js-> 
+ * a find se le pasa un callback que le ayude a buscar
+ *Includes-> leer documentacion
+*/
 
