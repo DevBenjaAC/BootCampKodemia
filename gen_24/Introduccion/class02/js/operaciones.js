72633d759ca9791f1ef92d495530a79c0088cf52
DevBenjaAC <benjaminalcis.xbox@gmail.com>
2023-02-22 19:36:17 -0600

    feat: add new class JS

--- gen_24/Introduccion/class02/js/operaciones.js
+++ gen_24/Introduccion/class02/js/operaciones.js
@@ -2,7 +2,7 @@ const myName = 'Benjamin';
 const myLastName = 'Alvarado Cisneros';
 let myAge = 27;
 let myWork = 'IoT Developer';
-let description = "Im IoT developer in 'Btob Consultores' i work to build the IoT platform";
+let description = "'Btob Consultores' i work to build the IoT platform";
 
 console.log(`My name is ${myName} ${myLastName}, i am ${myAge} years old, and i work as ${myWork} in ${description}`);
 
